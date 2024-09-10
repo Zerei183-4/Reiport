7791c163012a4bf8012e5bd623f11e4b290d3b28
Zerei <ogisango.rh@gmail.com>
2024-09-11 03:34:21 +0900

    Fix unescaped single quoptes in JSX

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Cybersecurity Enthusiast</span>
           <h1 className="h1">
-            Hello I'm <br /> <span className="text-accent">Rei Haga</span>
+            Hello I&#39;m <br /> <span className="text-accent">Rei Haga</span>
           </h1>
           <p className="max-w-[500px] mb-9 text-white/80">
             I excel at Cybersecurity defensive strategies on attack mitigation and prevention measures.
